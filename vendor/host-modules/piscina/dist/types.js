209f125ac0b9b40e07b9e486a35297dfc809dbe1
Zichuan Li <zichuan.li@outlook.com>
2026-09-21 20:44:43 -0500

    vendor: preserve Host worker and canvas dependencies
    Part of the native-layout local-runtime migration. Related source, dependencies and verification are grouped separately from documentation; no credentials or generated runtime state are included.

---
+++ vendor/host-modules/piscina/dist/types.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.commonState = void 0;
+exports.commonState = {
+    isWorkerThread: false,
+    workerData: undefined
+};
